5cb296ea02df01fb48794ba6a591e441b2ea723a
AlfredoVocos777 <alfevocos2@gmail.com>
2026-04-14 20:24:52 -0300

    Update db config

--- backEnd/configDB/dataBase.js
+++ backEnd/configDB/dataBase.js
@@ -3,8 +3,8 @@ import mysql from 'mysql2';
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '1234',
-    database: 'sigedex1',
+    password: 'Alfredvocos777',
+    database: 'sigedex',
     ssl: { rejectUnauthorized: false },
 });
 
