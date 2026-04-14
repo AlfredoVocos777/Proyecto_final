ea0cfbb6e759a995f6360db0558b25f0556e8633
AlfredoVocos777 <alfevocos2@gmail.com>
2026-04-14 03:16:31 -0300

    Flujo manual de pago en Mercado Pago

--- backEnd/routes/pagos.js
+++ backEnd/routes/pagos.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { formalizarTramiteYPago } from "../controllers/pagoExpediente.js";
+import { crearPreferencia } from "../controllers/mercadopagoController.js";
 
 const router = express.Router();
 
 // Ruta donde el frontend enviará TODOS los datos consolidados
 router.post("/", formalizarTramiteYPago); 
+
+// Ruta para inicializar el pago en Mercado Pago
+router.post("/crear-preferencia", crearPreferencia);
 
 export default router;
